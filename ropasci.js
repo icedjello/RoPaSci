c35d51a2d000a1000110e26f76310151fca19b74
Bamdad <bamfard@gmail.com>
2019-02-26 13:55:26 +0000

    Changed weapons from let to const.

--- ropasci.js
+++ ropasci.js
@@ -1,6 +1,6 @@
-function ropasco(myChoice, yourChoice) {
-    let weapons = ["paper", "rock", "scissors"]; // The options!
-    let choices = weapons[yourChoice] + " vs " + weapons[myChoice]; // So we don't have to repeat code. 
+function ropasci(myChoice, yourChoice) {
+    const WEAPONS = ["paper", "rock", "scissors"]; // The options!
+    let choices = WEAPONS[yourChoice] + " vs " + WEAPONS[myChoice]; // So we don't have to repeat code. 
 
     return (yourChoice == myChoice) ? choices + ", draw!" : // If same weapon > Draw! 
         (myChoice == (yourChoice - 1)%3) ? choices + ", I win!" : // I beat you! 
@@ -10,4 +10,4 @@ function ropasco(myChoice, yourChoice) {
 function myChooser() { // This has been placed outside for redundancy and symmetry.
     return Math.floor(Math.random() * 3);
 }
-console.log(ropasco(myChooser(),2));
+console.log(ropasci(myChooser(),2));
