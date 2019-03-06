bfc4dbcd1cb879490a70cc21dd09a98bfb89ea45
icedjello <bamfard@gmail.com>
2019-03-06 15:18:55 +0000

    Update index.js
    changed "==" to "===".
    Wrong type should throw an error.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function ropasci(myChoice, yourChoice) { "use strict";
     const WEAPONS = ["paper", "rock", "scissors"]; // The options!
     let choices = WEAPONS[yourChoice] + " vs " + WEAPONS[myChoice]; // So we don't have to repeat code. 
 
-    return (yourChoice == myChoice) ? choices + ", draw!" : // If same weapon > Draw! 
-        (myChoice == (yourChoice - 1)%3) ? choices + ", I win!" : // I beat you! 
+    return (yourChoice === myChoice) ? choices + ", draw!" : // If same weapon > Draw! 
+        (myChoice === (yourChoice - 1)%3) ? choices + ", I win!" : // I beat you! 
         choices + ", you win!"; // And if it isn't a draw and you haven't won...
 } 
 
